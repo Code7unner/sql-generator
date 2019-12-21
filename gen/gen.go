fd1b0754a356bfa0cdb9c71f7e172e72fa3c59ed
Maxim <cherbadzhimaxim@gmail.com>
2019-12-21 20:53:34 +1000

    Add generating info for db tables

--- gen/gen.go
+++ gen/gen.go
@@ -1,7 +1,9 @@
 package gen
 
 import (
+	"math/rand"
 	"time"
+	"github.com/brianvoe/gofakeit"
 )
 
 const (
@@ -9,6 +11,53 @@ const (
 	CATEGORIES_COUNT = 5000
 	USERS_COUNT = 500000
 )
+
+type Category struct {
+	Id       string
+	Name     string
+	ParentId string
+}
+
+func generateCategories() ([]Category, []string) {
+	var categoryIds []string
+	var categories []Category
+
+	for i := 0; i < CATEGORIES_COUNT; i++ {
+		c := Category{
+			Id: gofakeit.UUID(),
+			Name: gofakeit.Company(),
+		}
+
+		if i == 0 {	c.ParentId = c.Id } else { c.Id = categoryIds[len(categoryIds) - 1] }
+
+		categoryIds = append(categoryIds, c.Id)
+		categories = append(categories, c)
+	}
+
+	return categories, categoryIds
+}
+
+type User struct {
+	Id   string
+	Name string
+}
+
+func generateUsers() ([]User, []string) {
+	var userIds []string
+	var users []User
+
+	for i := 0; i < USERS_COUNT; i++ {
+		user := User{
+			Id:   gofakeit.UUID(),
+			Name: gofakeit.Name(),
+		}
+
+		userIds = append(userIds, user.Id)
+		users = append(users, user)
+	}
+
+	return users, userIds
+}
 
 type Message struct {
 	Id         string `fake:"{ misc.uuid}"`
@@ -18,13 +67,30 @@ type Message struct {
 	AuthorId   string
 }
 
-type Category struct {
-	Id       string
-	Name     string
-	ParentId string
+func generateMessages(userIds []string, categoryIds []string) []Message {
+	var messages []Message
+
+	for i := 0; i < MESSAGES_COUNT; i++ {
+		message := Message{
+			Id:         gofakeit.UUID(),
+			Text:       gofakeit.ProgrammingLanguage(),
+			CategoryId: categoryIds[rand.Intn(len(categoryIds))],
+			PostedAt:   gofakeit.Date(),
+			AuthorId:   userIds[rand.Intn(len(userIds))],
+		}
+
+		messages = append(messages, message)
+	}
+
+	return messages
 }
 
-type User struct {
-	Id   string
-	Name string
+func Generate() ([]Category, []User, []Message) {
+	gofakeit.Seed(time.Now().UnixNano())
+
+	categories, categoryIds := generateCategories()
+	users, userIds := generateUsers()
+	messages := generateMessages(userIds, categoryIds)
+
+	return categories, users, messages
 }
--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	_ "github.com/lib/pq"
+	"log"
 	db2 "sql-generator/db"
+	"sql-generator/gen"
+	"time"
 )
 
 func main()  {
+	startGenerationTime := time.Now().Unix()
+
 	db, err := db2.InitDatabase()
 	if err != nil {
 		panic(err)
@@ -16,4 +21,10 @@ func main()  {
 	if err != nil {
 		panic(err)
 	}
+
+	categories, users, messages := gen.Generate()
+
+	endGenerationTime := time.Now().Unix()
+
+	log.Printf("Generation data: %d seconds" , endGenerationTime - startGenerationTime)
 }
